f1c7f27a419aa9ebd4147cb2222349df6b5cd2e7
Jonny147 <jonathan.locke@iglu.com>
2019-02-18 23:30:04 +0000

    initial commit

---
+++ GoTimeDomain.Host/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoTimeDomain.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
